7a257ef66bbd7693ee6857fc7e8b83e9c2f366b0
Nayoh7467 <toasterstrudel50@gmail.com>
2022-11-30 14:22:58 -0600

    Added unimportant comment

--- src/main/java/net/nayoh/tutorialmod/tutorialmod.java
+++ src/main/java/net/nayoh/tutorialmod/tutorialmod.java
@@ -1,7 +1,6 @@
 package net.nayoh.tutorialmod;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -13,12 +12,13 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(tutorialmod.MOD_ID)
-public class tutorialmod {
+@Mod(TutorialMod.MOD_ID)
+public class TutorialMod {
     public static final String MOD_ID = "tutorialmod";
     private static final Logger LOGGER = LogUtils.getLogger();
-    // Very Important Comment
-    public tutorialmod()
+
+    // Very unimportant comment
+    public TutorialMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
@@ -31,15 +31,13 @@ public class tutorialmod {
 
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
         }
     }
 }
